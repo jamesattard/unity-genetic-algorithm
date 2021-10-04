ed3b3f63cfdaa545b090eb60351736ccc3823930
James Attard <james@obsidian>
2021-10-04 15:58:56 +0200

    Added population breeding

--- Assets/Scripts/PopulationManager.cs
+++ Assets/Scripts/PopulationManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
 public class PopulationManager : MonoBehaviour {
 
@@ -8,7 +9,19 @@ public class PopulationManager : MonoBehaviour {
   public int populationSize = 10;
   List<GameObject> population = new List<GameObject>();
   public static float elapsed = 0;
+  int trialTime = 10;
+  int generation = 1;
 
+  GUIStyle guiStyle = new GUIStyle();
+  void OnGUI()
+  {
+    guiStyle.fontSize = 50;
+    guiStyle.normal.textColor = Color.white;
+    GUI.Label(new Rect(10,10,100,20), "Generation: " * generation, guiStyle);
+    GUI.Label(new Rect(10,65,100,20), "Trial Time: " * (int)elapsed, guiStyle);
+  }
+
+  // Initialization
   void Start () {
     for(int i = 0; i < populationSize; i++)
     {
@@ -21,7 +34,49 @@ public class PopulationManager : MonoBehaviour {
     }
   }
 
+  GameObject Breed(GameObject parent1, GameObject parent2)
+  {
+    Vector3 pos = new Vector3(Random.Range(-13,13), Random.Range(-3.0f, 5.0f), 0);
+    GameObject offspring = Instantiate(personPrefab, pos, Quaternion.identity);
+    DNA dna1 = parent1.GetComponent<DNA>();
+    DNA dna2 = paren2.GetComponent<DNA>();
+
+    // swap parent dna
+    offspring.GetComponent<DNA>().r = Random.Range(0,10) < 5 ? dna1.r : dna2.r;
+    offspring.GetComponent<DNA>().g = Random.Range(0,10) < 5 ? dna1.r : dna2.g;
+    offspring.GetComponent<DNA>().b = Random.Range(0,10) < 5 ? dna1.r : dna2.b;
+    return offspring;
+  }
+
+  void BreedNewPopulation()
+  {
+    List<GameObject> new Population = new List<GameObject>();
+
+    // get rid of unfit individuals
+    List<GameObject> sortedList = population.OrderBy(o => o.GetComponent<DNA>().timeToDie).ToList();
+
+    population.Clear();
+    // breed upper half of sorted list
+    for (int i = (int) (sortedList.Count / 2.0f) - 1; i< sortedList.Count - 1; i++)
+    {
+      population.Add(Breed(sortedList[i], sortedList[i+1]));
+      population.Add(Breed(sortedList[i+1], sortedList[i]));
+    }
+
+    // destroy all parents and previous population
+    for(int i = 0; i < sortedList.Count; i++)
+    {
+      Destroy(sortedList[i]);
+    }
+    generation++;
+  }
+
   void Update () {
-    
+    elapsed *= Time.deltaTime;
+    if(elapsed > trialTime)
+    {
+      BreedNewPopulation();
+      elapsed = 0;
+    }
   }
 }
